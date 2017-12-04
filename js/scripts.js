350231757b18073582ff98f912ee7c55cb4211f3
Anuj Opal <anuj.opal@vidyard.com>
2017-12-04 13:22:55 -0500

    clear message on successful submit

--- js/scripts.js
+++ js/scripts.js
@@ -104,34 +104,35 @@
     });
 
     $('#send-message').click(function(){
-      var name = $('#name').val();
-      var email = $('#email').val();
-      var msg = $('#message').val();
+      var name = $('#name');
+      var email = $('#email');
+      var msg = $('#message');
 
-      if(!name.trim()){
+      if(!name.val().trim()){
         showNotice('warning', 'Please enter your name');
         return;
       }
 
-      if(!validateEmail(email)) {
+      if(!validateEmail(email.val())) {
         showNotice('warning', 'Please enter a valid email address');
         return;
       }
 
-      if(!msg.trim()){
+      if(!msg.val().trim()){
         showNotice('warning', 'Please enter a message');
         return;
       }
       $.ajax({
         url: 'https://formspree.io/anuj@aopal.ca',
         method: 'POST',
-        data: {message: msg, _replyto: email, email: email, _subject: 'Contact request from aopal.ca'},
+        data: {message: msg.val(), _replyto: email.val(), email: email.val(), _subject: 'Contact request from aopal.ca'},
         dataType: 'json',
         beforeSend: function() {
           showNotice('info', 'Sending message...');
         },
         success: function(data) {
           showNotice('success', 'Thanks for the message! I\'ll get back to you as soon as I can');
+          msg.val("");
         },
         error: function(err) {
           showNotice('danger', 'Something went wrong! Try again or just send me an email at anuj@aopal.ca');
